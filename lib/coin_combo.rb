223ca82bda82bde0f1b24aa590cb99fa0cd96a9c
Alexa Zeazas & Alisa Colon <student@epicodus.com>
2015-08-06 11:21:59 -0700

    Add code for returning number of quarters and dimes

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,7 +1,11 @@
 class Fixnum
   define_method(:coin_combo) do
+    change = []
 
     quarters = self./(25)
+    dimes = self.-(quarters.*(25))./(10)
+
+    change = [quarters, dimes]
 
   end
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -5,11 +5,11 @@ require('coin_combo')
 describe("Fixnum#coin_combo") do
 
   it("divides input by 25 and returns number of quarters necessary") do
-    expect((99).coin_combo()).to(eq(3))
+    expect((75).coin_combo()).to(eq([3, 0]))
   end
 
   it("processes input and returns number of quarters and dimes necessary") do
-    expect((99).coin_combo()).to(eq([3, 4]))
+    expect((95).coin_combo()).to(eq([3, 2]))
   end
 
 end
