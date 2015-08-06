dbce16a4472d64d08c891506dbd34f0378cfe4c2
Alexa Zeazas & Alisa Colon <student@epicodus.com>
2015-08-06 12:09:00 -0700

    Add integration specs.

---
+++ spec/coin_combo_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('launchy')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the coin combo path', {:type => :feature}) do
+  it('processes the user entry and returns the necessary number of quarters, dimes, nickels and pennies') do
+    visit('/')
+    fill_in('cents', :with => '41')
+    click_button('Send')
+    expect(page).to have_content('[1, 1, 1, 1]')
+  end
+end
