623dafbb7f5039ff1394c6aa2285b5154d998c13
Alexa Zeazas & Alisa Colon <student@epicodus.com>
2015-08-06 12:05:06 -0700

    Add gems.

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,12 +1,17 @@
 class Fixnum
   define_method(:coin_combo) do
-    change = []
+    change = ""
 
-    quarters = self./(25)
-    dimes = self.-(quarters.*(25))./(10)
-    nickels = self.-(quarters.*(25)).-(dimes.*(10))./(5)
-    pennies = self.-(quarters.*(25)).-(dimes.*(10)).-(nickels.*(5))./(1)
-    change = [quarters, dimes, nickels, pennies]
+    quarters = self./(25).to-s
+    dimes = self.-(quarters.*(25))./(10).to-s
+    nickels = self.-(quarters.*(25)).-(dimes.*(10))./(5).to-s
+    pennies = self.-(quarters.*(25)).-(dimes.*(10)).-(nickels.*(5))./(1).to-s
+
+    if quarters == "1"
+      quarters.concat(" quarter")
+    else
+      quarters.concat(" quarters")
+    end
 
   end
 end
