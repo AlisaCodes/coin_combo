fd3dc5834e709879cdef6ec233781aeefeb4634b
Alexa Zeazas & Alisa Colon <student@epicodus.com>
2015-08-06 10:52:13 -0700

    Add spec for returning number of quarters and dimes.

--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -4,8 +4,12 @@ require('coin_combo')
 
 describe("Fixnum#coin_combo") do
 
-  it("divides input by 25 and returns answer without remainder") do
+  it("divides input by 25 and returns number of quarters necessary") do
     expect((99).coin_combo()).to(eq(3))
+  end
+
+  it("processes input and returns number of quarters and dimes necessary") do
+    expect((99).coin_combo()).to(eq([3, 4]))
   end
 
 end
